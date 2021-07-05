8af8ac2e875ad26dac703d3bd4ae53bceb3bfea4
Thales Salazar <thalessalazar.12@gmail.com>
2021-07-05 20:46:17 -0300

    Aula 1

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    valor1: 0.0,
+    valor2: 0.0,
+    resultado: 0.0,
+  }
+
+  atualizaValor1 = (number) => {
+    this.setState({ valor1: number });
+  }
+
+  atualizaValor2 = (number) => {
+    this.setState({ valor2: number });
+  }
+
+  somar = (valor1, valor2) => {
+    this.state.resultado = 
+      parseFloat(this.state.valor1) + 
+      parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput 
+          onChangeText={this.atualizaValor1}
+          placeholder="Valor 1"
+          value={this.state.valor1}
+          />
+          
+        <TextInput 
+          onChangeText={this.atualizaValor2}
+          placeholder="Valor 1" 
+          value={this.state.valor2} />
+
+        <Button title="Somar" onPress={() => this.somar(this.state.valor1, this.state.valor2)} />
+      </View>
+    )
+  }
 }
+
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
